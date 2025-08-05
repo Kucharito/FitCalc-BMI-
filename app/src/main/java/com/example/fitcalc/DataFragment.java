b1cb59474298b4c2baf290212eaec709e9d105d0
Adam Kuchar <adamkuchar10@gmail.com>
2025-08-05 21:31:02 +0200

    Added bottomnav, fragments, Fragment graph to do

---
+++ app/src/main/java/com/example/fitcalc/DataFragment.java
@@ -0,0 +1,129 @@
+package com.example.fitcalc;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.cardview.widget.CardView;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+public class DataFragment extends Fragment {
+
+    private EditText vaha;
+    private EditText vyska;
+    private EditText vek;
+    private Button buttonBMI;
+    private TextView vysledokBMI;
+    private TextView textViewBMI;
+    private CardView cardResult;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.data_fragment, container, false);
+
+
+        vaha = view.findViewById(R.id.inputWeight);
+        vyska = view.findViewById(R.id.inputHeight);
+        vek = view.findViewById(R.id.inputAge);
+        buttonBMI = view.findViewById(R.id.buttonBMI);
+        vysledokBMI = view.findViewById(R.id.textView4);
+        textViewBMI = view.findViewById(R.id.textView5);
+        cardResult = view.findViewById(R.id.cardResult);
+
+
+        SharedPreferences sharedPreferences = requireContext().getSharedPreferences("FitCalcPrefs", MODE_PRIVATE);
+
+        // Obnovenie posledného BMI pri štarte aplikácie
+        float lastBMI = sharedPreferences.getFloat("lastBMI", 0.0f);
+        String lastBMItext = sharedPreferences.getString("lastBMItext", "Nebol vypočítaný BMI.");
+        if (lastBMI != 0.0f) {
+            //vysledokBMI.setText(String.format("Výsledok BMI: %.2f", lastBMI));
+            //textViewBMI.setText(lastBMItext);
+            //returnBMItext(lastBMI);
+        } else {
+            vysledokBMI.setText("Nebol vypočítaný BMI.");
+            textViewBMI.setText("Nebol vypočítaný BMI.");
+        }
+
+
+        buttonBMI.setOnClickListener(v -> {
+            String vahaText = vaha.getText().toString();
+            String vyskaText = vyska.getText().toString();
+            String vekText = vek.getText().toString();
+
+            if (!vahaText.isEmpty() && !vyskaText.isEmpty() && !vekText.isEmpty()) {
+                double hmotnost = Double.parseDouble(vahaText);
+                double vyskaCm = Double.parseDouble(vyskaText);
+                double vyskaM = vyskaCm / 100;
+                double bmi = hmotnost / (vyskaM * vyskaM);
+
+                String bmiText = String.format("Výsledok BMI: %.2f", bmi);
+                String bmiDesc = returnBMItext(bmi);
+
+                vysledokBMI.setText(bmiText);
+                textViewBMI.setText(bmiDesc);
+
+                // 💾 Uloženie bez parsovania stringu
+                SharedPreferences prefs = requireContext().getSharedPreferences("FitCalcPrefs", MODE_PRIVATE);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putFloat("lastBMI", (float) bmi);
+                editor.putString("lastBMItext", bmiDesc);
+                editor.apply();
+            } else {
+                vysledokBMI.setText("Prosím, vyplňte všetky polia.");
+                textViewBMI.setText("Nebol vypočítaný BMI.");
+            }
+        });
+        return view;
+    }
+
+
+
+    public String returnBMItext(double bmi) {
+        if (bmi < 16) {
+            cardResult.setCardBackgroundColor(Color.parseColor("#FFEB3B"));
+            return "Popis: Ťažká podváha";
+        }
+        else if(bmi <17){
+            cardResult.setCardBackgroundColor(Color.parseColor("#FFF176")); // žltá
+            return "Popis: Stredná podváha";
+        }
+        else if(bmi < 18.5){
+            cardResult.setCardBackgroundColor(Color.parseColor("#FFFDE7"));
+            return "Popis: Ľahká podváha";
+        }
+        else if(bmi < 25){
+            cardResult.setCardBackgroundColor(Color.parseColor("#A5D6A7"));
+            return "Popis: Normálna hmotnosť";
+        }
+        else if(bmi < 30){
+            cardResult.setCardBackgroundColor(Color.parseColor("#FFD54F"));
+            return "Popis: Nadváha";
+        }
+        else if(bmi < 35){
+            cardResult.setCardBackgroundColor(Color.parseColor("#EF9A9A"));
+            return "Popis: I. stupeň obezity";
+        }
+        else if(bmi < 40){
+            cardResult.setCardBackgroundColor(Color.parseColor("#E57373"));
+            return "Popis: II. stupeň obezity";
+        }
+        else{
+            cardResult.setCardBackgroundColor(Color.parseColor("#D32F2F"));
+            return "Popis: III. stupeň obezity";
+        }
+    }
+}
--- app/src/main/java/com/example/fitcalc/MainActivity.java
+++ app/src/main/java/com/example/fitcalc/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.fitcalc;
 
-import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,122 +18,58 @@ import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText vaha;
-    private EditText vyska;
-    private EditText vek;
-    private Button buttonBMI;
-    private TextView vysledokBMI;
-    private TextView textViewBMI;
-    private CardView cardResult;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
+    getSupportFragmentManager().beginTransaction()
+            .replace(R.id.fragment_container, new DataFragment())
+            .commit();
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
 
-
-
-        vaha = findViewById(R.id.inputWeight);
-        vyska = findViewById(R.id.inputHeight);
-        vek = findViewById(R.id.inputAge);
-        buttonBMI = findViewById(R.id.buttonBMI);
-        vysledokBMI = findViewById(R.id.textView4);
-        textViewBMI = findViewById(R.id.textView5);
-        cardResult = findViewById(R.id.cardResult);
-
-
-        SharedPreferences sharedPreferences = getSharedPreferences("FitCalcPrefs", MODE_PRIVATE);
-
-        // Obnovenie posledného BMI pri štarte aplikácie
-        float lastBMI = sharedPreferences.getFloat("lastBMI", 0.0f);
-        String lastBMItext = sharedPreferences.getString("lastBMItext", "Nebol vypočítaný BMI.");
-        if (lastBMI != 0.0f) {
-            //vysledokBMI.setText(String.format("Výsledok BMI: %.2f", lastBMI));
-            //textViewBMI.setText(lastBMItext);
-            //returnBMItext(lastBMI);
-        } else {
-            vysledokBMI.setText("Nebol vypočítaný BMI.");
-            textViewBMI.setText("Nebol vypočítaný BMI.");
-        }
-
-
-        buttonBMI.setOnClickListener(view -> {
-            String vahaText = vaha.getText().toString();
-            String vyskaText = vyska.getText().toString();
-            String vekText = vek.getText().toString();
-
-            if (!vahaText.isEmpty() && !vyskaText.isEmpty() && !vekText.isEmpty()) {
-                double hmotnost = Double.parseDouble(vahaText);
-                double vyskaCm = Double.parseDouble(vyskaText);
-                double vyskaM = vyskaCm / 100;
-                double bmi = hmotnost / (vyskaM * vyskaM);
-
-                String bmiText = String.format("Výsledok BMI: %.2f", bmi);
-                String bmiDesc = returnBMItext(bmi);
-
-                vysledokBMI.setText(bmiText);
-                textViewBMI.setText(bmiDesc);
-
-                // 💾 Uloženie bez parsovania stringu
-                SharedPreferences prefs = getSharedPreferences("FitCalcPrefs", MODE_PRIVATE);
-                SharedPreferences.Editor editor = prefs.edit();
-                editor.putFloat("lastBMI", (float) bmi);
-                editor.putString("lastBMItext", bmiDesc);
-                editor.apply();
-            } else {
-                vysledokBMI.setText("Prosím, vyplňte všetky polia.");
-                textViewBMI.setText("Nebol vypočítaný BMI.");
+            if (item.getItemId() == R.id.nav_data) {
+                selectedFragment = new DataFragment();
+            } else if (item.getItemId() == R.id.nav_graph) {
+                selectedFragment = new GraphFragment();
             }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+            return true;
         });
-
-
-
     }
 
-    public String returnBMItext(double bmi) {
-        if (bmi < 16) {
-            cardResult.setCardBackgroundColor(Color.parseColor("#FFEB3B"));
-            return "Popis: Ťažká podváha";
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            View view = getCurrentFocus();
+            if (view instanceof EditText) {
+                Rect outRect = new Rect();
+                view.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int) ev.getRawX(), (int) ev.getRawY())) {
+                    view.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                }
+            }
         }
-        else if(bmi <17){
-            cardResult.setCardBackgroundColor(Color.parseColor("#FFF176")); // žltá
-            return "Popis: Stredná podváha";
-        }
-        else if(bmi < 18.5){
-            cardResult.setCardBackgroundColor(Color.parseColor("#FFFDE7"));
-            return "Popis: Ľahká podváha";
-        }
-        else if(bmi < 25){
-            cardResult.setCardBackgroundColor(Color.parseColor("#A5D6A7"));
-            return "Popis: Normálna hmotnosť";
-        }
-        else if(bmi < 30){
-            cardResult.setCardBackgroundColor(Color.parseColor("#FFD54F"));
-            return "Popis: Nadváha";
-        }
-        else if(bmi < 35){
-            cardResult.setCardBackgroundColor(Color.parseColor("#EF9A9A"));
-            return "Popis: I. stupeň obezity";
-        }
-        else if(bmi < 40){
-            cardResult.setCardBackgroundColor(Color.parseColor("#E57373"));
-            return "Popis: II. stupeň obezity";
-        }
-        else{
-            cardResult.setCardBackgroundColor(Color.parseColor("#D32F2F"));
-            return "Popis: III. stupeň obezity";
-        }
+        return super.dispatchTouchEvent(ev);
     }
 }
