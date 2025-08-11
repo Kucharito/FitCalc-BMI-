a6cd4628c946133cd45bba5553eb0f77c5231fba
Adam Kuchar <adamkuchar10@gmail.com>
2025-08-11 19:03:24 +0200

    Added animations

--- app/src/main/java/com/example/fitcalc/MainActivity.java
+++ app/src/main/java/com/example/fitcalc/MainActivity.java
@@ -58,7 +58,14 @@ public class MainActivity extends AppCompatActivity {
 
                 if (selectedFragment != null) {
                     getSupportFragmentManager().beginTransaction()
+                            .setCustomAnimations(
+                                    R.anim.slide_in_right,
+                                    R.anim.slide_out_left,
+                                    R.anim.slide_in_left,
+                                    R.anim.slide_out_right
+                            )
                             .replace(R.id.fragment_container, selectedFragment)
+                            .addToBackStack(null)
                             .commit();
                 }
                 return true;
